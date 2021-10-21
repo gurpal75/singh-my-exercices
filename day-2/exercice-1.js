c5609576cccd0596cbf879bf6510adfaef122276
gurpal75 <gurpal.singh@epita.fr>
2021-10-21 16:21:16 +0200

    correction exo 1&2

--- day-2/exercice-1.js
+++ day-2/exercice-1.js
@@ -1,15 +1,8 @@
 export const my_alpha_reverse = (str) => {
-    let i = 0
-    let strng = ""
-    while (str[i] != undefined) {
-        i++
-    }
-    let length = 0
-    i -= 1
-    while (length >= 0) {
-        strng += str[i]
-        i -= 1
-        length += 1
-    }
-    return strng
+    var res = "";
+  var i = 0;
+  while (str[i] != undefined) {
+    res = str[i] + res;
+  }
+  return res;
 }
--- day-2/exercice-3.js
+++ day-2/exercice-3.js
@@ -1,3 +1,14 @@
 export const my_string_is_number = (str) => {
-    return /\d/.test(str)
+    if (str == null ||str == undefined) {
+        return false;
+      }
+    
+      var i = 0;
+      while (str[i] != undefined) {
+        if (str[i] >= '0' && str[i] <= '9') {
+          return true;
+        }
+        i++;
+      }
+      return false;
 }
